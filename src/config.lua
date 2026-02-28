819ea95dff5a9887fdd29d97840572aa05b3905a
Swaler <>
2026-02-28 22:52:41 +0300

    123123123

--- src/config.lua
+++ src/config.lua
@@ -1,22 +1,22 @@
 config = {
-    -- -- Формиат натрия
-    -- { item_id = "sodium formate",          min = 0, batch = 1 },
+    -- Формиат натрия
+    { item_id = "sodium formate",          min = 5 * 1000 * 1000, batch = 100 * 1000 },
     -- Соляная кислота
-    { item_id = "hydrochloricacid_gt5u",   min = 50 * 1000, batch = 10 * 1000 },
-    -- -- Угарный газ
-    -- { item_id = "carbonmonoxide",          min = 0, batch = 1 },
-    -- -- Аммиак
-    -- { item_id = "ammonia",                 min = 0, batch = 1 },
-    -- -- Муравьиная кислота
-    -- { item_id = "formic acid",             min = 0, batch = 1 },
-    -- -- Азотная кислота
-    -- { item_id = "nitricacid",              min = 0, batch = 1 },
-    -- -- Серная кислота
-    -- { item_id = "sulfuricacid",            min = 0, batch = 1 },
-    -- -- Хлорид аммония
-    -- { item_id = "ammonium chloride",       min = 0, batch = 1 },
-    -- -- Пыль гидроксида натрия
-    -- { item_id = "gregtech:gt.metaitem.01", min = 0, batch = 1 },
+    { item_id = "hydrochloricacid_gt5u",   min = 5 * 1000 * 1000, batch = 100 * 1000 },
+    -- Угарный газ
+    { item_id = "carbonmonoxide",          min = 5 * 1000 * 1000, batch = 100 * 1000 },
+    -- Аммиак
+    { item_id = "ammonia",                 min = 5 * 1000 * 1000, batch = 100 * 1000 },
+    -- Муравьиная кислота
+    { item_id = "formic acid",             min = 5 * 1000 * 1000, batch = 100 * 1000 },
+    -- Азотная кислота
+    { item_id = "nitricacid",              min = 5 * 1000 * 1000, batch = 100 * 1000 },
+    -- Серная кислота
+    { item_id = "sulfuricacid",            min = 5 * 1000 * 1000, batch = 100 * 1000 },
+    -- Хлорид аммония
+    { item_id = "ammonium chloride",       min = 5 * 1000 * 1000, batch = 100 * 1000 },
+    -- Пыль гидроксида натрия
+    { item_id = "gregtech:gt.metaitem.01", min = 5 * 1000 * 1000, batch = 100 * 1000 },
 }
 
 return config
